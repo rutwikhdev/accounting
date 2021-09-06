3b598a3c4e5c780b57c12e60d4d48127606fcb55
Rutwik Hiwalkar <rutwikhdev@gmail.com>
2021-09-06 15:23:46 +0530

    feat: added reports for balance sheet and pnl

---
+++ accounting/accounting/report/balance_sheet/balance_sheet.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2013, Rutwik and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from accounting.accounting.report.financial_statements import get_data, get_columns
+from frappe import _
+
+def execute(filters=None):
+    data = []
+    asset = get_data('Asset', 'Debit', filters.from_date, filters.to_date)
+    liability = get_data('Liability', 'Debit', filters.from_date, filters.to_date)
+    equity = get_data('Equity', 'Credit', filters.from_date, filters.to_date)
+
+    data.extend(asset or [])
+    data.extend(liability or [])
+    data.extend(equity or [])
+
+    columns = get_columns()
+    chart = get_chart_data(columns, asset, liability, equity)
+    print("Chart Data: ", chart)
+    return columns, data, None, chart
+
+def get_chart_data(columns, asset, liability, equity):
+    labels = [d.get("label") for d in columns[2:]]
+
+    asset_data, liability_data, equity_data = [], [], []
+
+    for p in columns[1:]:
+        if asset:
+            asset_data.append(asset[-2].get(p.get("fieldname")))
+        if liability:
+            liability_data.append(liability[-2].get(p.get("fieldname")))
+        if equity:
+            equity_data.append(equity[-2].get(p.get("fieldname")))
+        print("P is: ", p)
+
+
+    datasets = []
+    if asset_data:
+        datasets.append({'name': _('Assets'), 'values': asset_data})
+    if liability_data:
+        datasets.append({'name': _('Liabilities'), 'values': liability_data})
+    if equity_data:
+        datasets.append({'name': _('Equity'), 'values': equity_data})
+
+    chart = {
+        "data": {
+            'labels': labels,
+            'datasets': datasets
+        }
+    }
+
+    chart["type"] = "bar"
+
+    return chart
---
+++ accounting/accounting/report/profit_and_loss/profit_and_loss.py
@@ -0,0 +1,108 @@
+# Copyright (c) 2013, Rutwik and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from frappe import _
+from frappe.utils import flt
+
+from accounting.accounting.report.financial_statements import get_data, get_columns
+
+def execute(filters=None):
+    columns, data = [], []
+    income = get_data('Income', 'Credit', filters.from_date, filters.to_date)
+    expense = get_data('Expense', 'Debit', filters.from_date, filters.to_date)
+
+    data.extend(income or [])
+    data.extend(expense or [])
+    print('Data: ', data)
+
+    net_profit_loss = get_net_profit_loss(income, expense)
+
+    if net_profit_loss:
+        data.append(net_profit_loss)
+
+    columns = get_columns()
+    chart = get_chart_data(columns, income, expense, net_profit_loss)
+
+    return columns, data
+
+def get_net_profit_loss(income, expense):
+    total = 0
+    net_profit_loss = {
+        "account_name": "'" + _("Profit for the year") + "'",
+        "account": "'" + _("Profit for the year") + "'",
+        "warn_if_negative": True,
+    }
+
+    total_income = flt(income[-2]['opening_balance'], 3) if income else 0
+    total_expense = flt(expense[-2]['opening_balance'], 3) if expense else 0
+
+    net_profit_loss['opening_balance'] = total_income - total_expense
+
+    return net_profit_loss
+
+def get_report_summary(income, expense, net_profit_loss):
+    net_income, net_expense, net_profit = 0.0, 0.0, 0.0
+
+    net_income = flt(income[-2]['opening_balance'], 3)
+    net_expense = flt(expense[-2]['opening_balance'], 3)
+    net_profit = flt(net_profit_loss[-2]['opening_balance'], 3)
+
+    profit_label = _('Net Profit')
+    income_label = _('Total Income')
+    expense_label = _('Total Expense')
+
+    return [
+        {
+            'value': net_income,
+            'label': income_label,
+            'datatype': 'Currency'
+        },
+        { 'type': 'separator', 'value': '-' },
+        {
+            'value': net_expense,
+            'label': expense_label,
+            'datatype': 'Currency'
+        },
+        { 'type': 'separator', 'value': '=', 'color': 'blue' },
+        {
+            'value': net_profit,
+            'indicator': 'Green' if net_profit > 0 else 'Red',
+            'label': profit_label,
+            'datatype': 'Currency'
+        }
+    ]
+
+def get_chart_data(columns, income, expense, net_profit_loss):
+    labels = [d.get("label") for d in columns[2:]]
+
+    income_data, expense_data, net_profit = [], [], []
+
+    for p in columns[2:]:
+        if income:
+            income_data.append(income[-2].get(p.get("fieldname")))
+        if expense:
+            expense_data.append(expense[-2].get(p.get("fieldname")))
+        if net_profit_loss:
+            net_profit.append(net_profit_loss.get(p.get("fieldname")))
+
+    datasets = []
+    if income_data:
+        datasets.append({'name': _('Income'), 'values': income_data})
+    if expense_data:
+        datasets.append({'name': _('Expense'), 'values': expense_data})
+    if net_profit:
+        datasets.append({'name': _('Net Profit/Loss'), 'values': net_profit})
+
+    chart = {
+        "data": {
+            'labels': labels,
+            'datasets': datasets
+        }
+    }
+
+    chart["type"] = "bar"
+    chart["fieldtype"] = "Currency"
+
+    return chart
